35a9617697fada13f4474b646135e6d8c8a7bb16
Amalia Wahyu <uamaliawahyu@gmail.com>
2026-02-09 16:47:59 +0700

    add descriptive comments in analog_io.h, digital_io.c, digital_io.h, external_interrupts.c, and external_interrupts.h

--- arduino/cores/digital_io.c
+++ arduino/cores/digital_io.c
@@ -4,7 +4,10 @@
 
 #include "digital_io.h"
 
+/* Validates pin range and returns the mapped physical pad index*/
 static inline uint32_t digitalPinToPad(uint8_t pin){
+
+	/* Ensure the pin index doesn't exceed the mapping table's size*/
     if (pin >= (sizeof(digital_pin_to_pad) / sizeof(digital_pin_to_pad[0])))
         return NOT_A_PIN;
     return digital_pin_to_pad[pin];
@@ -12,31 +15,47 @@ static inline uint32_t digitalPinToPad(uint8_t pin){
 
 static inline void setPadmuxToGPIO(uint32_t pad_index) {
     uint32_t reg_addr;
+
+	/* Calculate bit position: each pad uses 2 bits, so we shift by (index % 16) * 2 */
     uint32_t shift = (pad_index % 16) * 2;
+
+	/* Select the appropriate register: PADMUX_0 for pads 0-15, PADMUX_1 for 16-31 */
     if (pad_index < 16) {
 		reg_addr = PADMUX_0;
 	}	else {
 			reg_addr = PADMUX_1;
 		}
     
+	/* Read the current register value from hardware*/
     uint32_t reg_val = pulp_read32(reg_addr);
+
     /*reg_val &= ~(0b11 << shift);
     reg_val |= (0b01 << shift);*/
     reg_val &= ~(0x3 << shift);
     reg_val |=  (0x1 << shift);
+
+	/* Write the modified configuration back to the hardware register*/
     pulp_write32(reg_addr, reg_val);
 }
 
 int digitalRead(int pin){
+	/* switch pad function from default to GPIO mode */
     setPadmuxToGPIO(digitalPinToPad(pin));
+
+	/* read the current state of all GPIO pins from hardware register*/
 	uint32_t padin = hal_gpio_get_value();
+
+	/* mask the register value to isolate the specific bit for this pin*/
 	int val = padin & (1 << pin);
+
+	/* return standard Arduino HIGH (1) or LOW (0) based on bit state*/
 	return val ? HIGH : LOW;
 }
 
 void digitalWrite(int pin, int value){
-    setPadmuxToGPIO(digitalPinToPad(pin));
+	setPadmuxToGPIO(digitalPinToPad(pin));
 
+	/* apply the logic level to the hardware output register*/
 	if (value == LOW) {
 		hal_gpio_set_pin_value(pin, 0);
 	} else {
@@ -48,10 +67,13 @@ void pinMode(int pin, uint8_t mode){
     uint32_t pad = digitalPinToPad(pin);
 	if (pad == NOT_A_PIN) return;
 
+	/* connect physical pad to GPIO controller*/
 	setPadmuxToGPIO(pad);
 
+	/* enable the specific GPIO pin*/
 	hal_gpio_en_set(1<<pin);
 
+	/* set data direction: 1 for OUTPUT, 0 for INPUT*/
 	if (mode == OUTPUT) {
 		hal_gpio_set_dir(1<<pin, 1);
 	} else {
--- arduino/cores/external_interrupts.h
+++ arduino/cores/external_interrupts.h
@@ -1,3 +1,34 @@
+/**
+ * function attachInterrupts
+ * Interrupts help make things happen automatically in microcontroller programs 
+ * and can help solve timing problems. Good tasks for using an interrupt may 
+ * include reading a rotary encoder, or monitoring user input. If you wanted to 
+ * ensure that a program always caught the pulses from a rotary encoder, so that 
+ * it never misses a pulse, it would make it very tricky to write a program to do 
+ * anything else, because the program would need to constantly poll the sensor 
+ * lines for the encoder, to catch pulses when they occurred. Other sensors have 
+ * a similar interface dynamic, such as trying to read a sound sensor that 
+ * detects a click, or an infrared slot sensor (photo-interrupter) that detects 
+ * a coin drop. In all these situations, using an interrupt can free the 
+ * microcontroller to do other work while still capturing the input.
+ * 
+ * https://docs.arduino.cc/language-reference/en/functions/external-interrupts/attachInterrupt/
+ */
 void attachInterrupt(int interrupt, void(*userFunc)(void), int mode);
+
+/**
+ * function detachInterrupt
+ * This function turns off a given interrupt that was attached previously.
+ *
+ * https://docs.arduino.cc/language-reference/en/functions/external-interrupts/detachInterrupt/
+ */
 void detachInterrupt(int interrupt);
+
+/**
+ * function digitalPinToInterrupt
+ * The digitalPinToInterrupt() function takes a pin as an argument, and returns
+ * the same pin if it can be used as an interrupt.
+ * 
+ * https://docs.arduino.cc/language-reference/en/functions/external-interrupts/digitalPinToInterrupt/
+ */
 int digitalPinToInterrupt(int pin);
