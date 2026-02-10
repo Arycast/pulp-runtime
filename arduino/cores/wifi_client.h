cad1341285dd8487013c1e635796d2301cbf5c10
Alfi Dzikro Hasibuan <alfidhsb@gmail.com>
2026-02-10 16:53:50 +0700

    Add descriptive comments in wifi_client.h and wifi_server.h

--- arduino/cores/wifi_client.h
+++ arduino/cores/wifi_client.h
@@ -1,47 +1,127 @@
+/**
+    * this file is implementation of Arduino WiFi Client API
+    * https://docs.arduino.cc/language-reference/en/functions/wifi/client/
+*/
+
 #ifndef wifi_client_h
 #define wifi_client_h
 
-#include <stdbool.h>
-#include <stddef.h>
+#include <stdbool.h> /* provides boolean type support */
+#include <stddef.h> /* provides standard definitions */
 
-#include "variables.h"
+#include "variables.h" 
 #include "String.hpp"
-#include "IPAddress.h"
-#include "stream.h"
+#include "IPAddress.h" /* Support for 4-byte IP Address structures */
+#include "stream.h" /* base class for character and binary based streams */
 
+/**
+    * class WiFiClient
+    * inherits from IPAddress and Stream class
+    */
 class WiFiClient : public IPAddress, public Stream
 {
 public :
-    // override virtual method (from Stream class)
-    int available(void) override;
-    int read(void) override;
-    void flush(void) override;
-    int peek(void) override;
+    /**
+        * public class method
+        */
 
+    /** 
+        * method available, read, and flush
+        * override class from stream class because WiFi client has its own implementation
+        */
+    
+    /**
+        * method available
+        * returns the amount of data that has been written to the client by the server is connected to (bytes available for reading)
+        * @return The number of bytes available
+        * https://docs.arduino.cc/language-reference/en/functions/wifi/client/#clientavailable
+        */
+    int available(void) override;
+
+    /**
+        * method read
+        * read the next byte received from the server client is connected to
+        * @return The next byte (or -1 if none is available)
+        * https://docs.arduino.cc/language-reference/en/functions/wifi/client/#clientread
+        */
+    int read(void) override;
+
+    /**
+        * method flush
+        * discards any bytes that have been written to the client but not yet read
+        * https://docs.arduino.cc/language-reference/en/functions/wifi/client/#clientflush
+        */
+    void flush(void) override;
+
+    /*void peek(void) override;*/ /*standard dont have peek method*/
+
+    /**
+        * method connected
+        * whether or not the client is connected
+        * note that a client is considered connected if the connection has been closed but there is still unread data
+        * @return True if the client is connected, false if not
+        * https://docs.arduino.cc/language-reference/en/functions/wifi/client/#clientconnected 
+        */
     bool connected(void);
+
+    /**
+        * method connect
+        * connect to the IP address and port specified in the constructor
+        * connect() also supports DNS lookups when using a domain name
+        * @param ip The IP Address that the client will connect to
+        * @param port The port that the client will connect to
+        * @param URL The domain name that the client will connected to
+        * @return True if the connection succeeds, false if not
+        */
     bool connect(IPAddress ip, int port);
     bool connect(String URL, int port);
+
+    /**
+        * method write
+        * write data to the server the client is connected to
+        * @param data The byte or char to write
+        * @return The number of characters written
+        * https://docs.arduino.cc/language-reference/en/functions/wifi/client/#clientwrite
+        */
     size_t write(byte data);
     /*byte write(byte data);*/ /* cannot overload function solely based on return type */
     size_t write(char data);
     /*byte write(char data);*/
+
+    /**
+        * method print
+        * print data to the server that a client is connected to
+        * @param data The data to print
+        * @param BASE The base in which to print numbers
+        * @return The number of bytes written, though reading that number is optional
+        * https://docs.arduino.cc/language-reference/en/functions/wifi/client/#clientprint
+        */
     byte print(char data);
     byte print(byte data);
     byte print(int data);
     byte print(long data);
     byte print(String data);
-    byte print(char data, int BASE); //DEC for decimal (base 10), OCT for octal (base 8), HEX for hexadecimal (base 16).
+    byte print(char data, int BASE);
     byte print(byte data, int BASE);
     byte print(int data, int BASE);
     byte print(long data, int BASE);
     byte print(String data, int BASE);
+
+    /**
+        * method println
+        * print data, followed by a carriage return and newline, to the server a client is connected to
+        * @param data The data to print
+        * @param BASE The base in which to print numbers
+        * @return The number of bytes written, though reading that number is optional
+        * https://docs.arduino.cc/language-reference/en/functions/wifi/client/#clientprint
+        */
     byte println(void);
     byte println(char data);
     byte println(byte data);
     byte println(int data);
     byte println(long data);
     byte println(String data);
-    byte println(char data, int BASE); //DEC for decimal (base 10), OCT for octal (base 8), HEX for hexadecimal (base 16).
+    byte println(char data, int BASE);
     byte println(byte data, int BASE);
     byte println(int data, int BASE);
     byte println(long data, int BASE);
@@ -49,6 +129,9 @@ public :
     void stop(void);
 };
 
+/**
+    * Pre-instantiated object for global access to the WiFiClient
+    */
 extern WiFiClient client;
 
 #endif
--- arduino/cores/wifi_server.h
+++ arduino/cores/wifi_server.h
@@ -1,41 +1,113 @@
+/**
+    * this file is implementation of Arduino WiFi Client API
+    * https://docs.arduino.cc/language-reference/en/functions/wifi/server/
+*/
+
 #ifndef wifi_server_h
 #define wifi_server_h
 
 #include "variables.h"
 #include "String.hpp"
 
-class WiFiServer {
+#include "wifi_client.h" /* provide WiFiClient object for return data type available method */
+
+/**
+    * class WiFiServer
+    * inherits from Stream class
+    */
+class WiFiServer : public Stream
+{
 public :
+    /**
+        * public class method
+        */
+
+    /**
+        * default constructor
+        */
     WiFiServer(void);
+
+    /**
+        * parameterized constructor
+        * @param port The port to listen on
+        * https://docs.arduino.cc/language-reference/en/functions/wifi/server/#wifiserver
+        */
     WiFiServer(int port);
+
+    /**
+        * destructor 
+        */
     ~WiFiServer(void);
 
+    /**
+        * method begin
+        * tells the server to begin listening for incoming connections
+        * https://docs.arduino.cc/language-reference/en/functions/wifi/server/#serverbegin
+        */
     void begin(void);
+
+    /**
+        * method write
+        * write data to all the clients connected to a server
+        * @param data The value to write
+        * @return The number of bytes written
+        * https://docs.arduino.cc/language-reference/en/functions/wifi/server/#serverwrite
+        */
     byte write(byte data);
     byte write(char data);
+
+    /**
+        * method print
+        * print data to all the clients connected to a server
+        * @param data The data to print
+        * @param BASE The base in which to print numbers
+        * @return The number of bytes written, though reading that number is optional
+        * https://docs.arduino.cc/language-reference/en/functions/wifi/server/#serverprint
+        */
     byte print(char data);
     byte print(byte data);
     byte print(int data);
     byte print(long data);
     byte print(String data);
-    byte print(char data, int BASE); //DEC for decimal (base 10), OCT for octal (base 8), HEX for hexadecimal (base 16).
+    byte print(char data, int BASE);
     byte print(byte data, int BASE);
     byte print(int data, int BASE);
     byte print(long data, int BASE);
     byte print(String data, int BASE);
+
+    /**
+        * method println
+        * prints data, followed by a newline, to all the clients connected to a server
+        * @param data The data to print
+        * @param BASE The base in which to print numbers
+        * @return The number of bytes written, though reading that number is optional
+        * https://docs.arduino.cc/language-reference/en/functions/wifi/server/#serverprintln
+        */
     byte println(void);
     byte println(char data);
     byte println(byte data);
     byte println(int data);
     byte println(long data);
     byte println(String data);
-    byte println(char data, int BASE); //DEC for decimal (base 10), OCT for octal (base 8), HEX for hexadecimal (base 16).
+    byte println(char data, int BASE);
     byte println(byte data, int BASE);
     byte println(int data, int BASE);
     byte println(long data, int BASE);
     byte println(String data, int BASE);
+
+    /**
+        * gets a client that is connected to the server and has data available for reading
+        * the connection persists when the returned client object goes out of scope
+        * closed by calling client.stop()
+        * @return a client object (this object will evaluate to false in an if-statement if no Client has data available for reading)
+        * https://docs.arduino.cc/language-reference/en/functions/wifi/server/#serveravailable
+        */
+    WiFiClient available(void) override;
 };
 
+/**
+    * Pre-instantiated object for global access to the WiFiServer
+    */
 extern WiFiServer server;
 
 #endif
